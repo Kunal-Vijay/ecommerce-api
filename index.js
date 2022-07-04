83fade552946446b339201bbd2953b64a4280a03
Kunal <kunal20vijay00@gmail.com>
2022-07-04 19:12:22 +0530

    connecting to mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-app.listen(5000, () => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to DB Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is running");
 });
