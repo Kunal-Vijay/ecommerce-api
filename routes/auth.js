652b4a5b789c0287740323ce1a603611deb8034b
Kunal <kunal20vijay00@gmail.com>
2022-07-06 18:34:56 +0530

    Implementing JWT

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 // Loading the environment variables
 dotenv.config();
@@ -35,17 +36,25 @@ router.post("/login", async (req, res) => {
 
     !user && res.status(401).send("Wrong credentials");
 
-    // decrypting password 
+    // decrypting password
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
       process.env.PASS_SEC
     );
     const pwd = hashedPassword.toString(CryptoJS.enc.Utf8);
     pwd !== req.body.password && res.status(401).send("Wrong credentials");
-    
-   
+
+    const accessWebToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
     const { password, ...others } = user._doc; // desturcturing user
-    res.status(200).json(others);  // sending user info other than password
+    res.status(200).json({ ...others, accessWebToken }); // sending user info other than password
   } catch (err) {
     res.status(500).send("Internal server error");
   }
