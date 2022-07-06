25ebaa429214138af95872eb6ce67c2d98b8e2eb
Kunal <kunal20vijay00@gmail.com>
2022-07-06 20:36:42 +0530

    Deleting user and getting user details

--- middleware/verifytoken.js
+++ middleware/verifytoken.js
@@ -24,4 +24,18 @@ const verifyTokenAndAuthorization = (req, res, next) => {
   });
 };
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(403).json("Not allowed : Permission denied");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
