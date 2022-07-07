c93a221b96af6e14df50c4dd7b6ae9d1d8796b93
Kunal <kunal20vijay00@gmail.com>
2022-07-07 20:00:21 +0530

    Order and Cart - CRUD

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 // Loading the environment variables
 dotenv.config();
@@ -26,6 +28,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
 
 // Listening connections
 app.listen(process.env.PORT || 5000, () => {
