9adba0877ac870e2360dd1181baebe85df8ffe24
Kunal <kunal20vijay00@gmail.com>
2022-07-07 18:33:10 +0530

    Product - CRUD operations

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 // Loading the environment variables
 dotenv.config();
@@ -24,6 +25,7 @@ app.use(express.json());
 // ENDPOINTS
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 // Listening connections
 app.listen(process.env.PORT || 5000, () => {
