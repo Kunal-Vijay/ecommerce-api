681a5e1f907b322b456f75a11ad1e8bb205f6d0c
Kunal <kunal20vijay00@gmail.com>
2022-07-07 18:36:02 +0530

    Products - CRUD operations

--- routes/product.js
+++ routes/product.js
@@ -18,7 +18,7 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 });
 
 // UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const updatedProduct = await Product.findByIdAndUpdate(
       req.params.id,
